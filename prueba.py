8c28f84ceab71836e96c96008d32edaad1629928
EduVillaa <eduardovllvrd@gmail.com>
2025-11-20 14:25:21 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -15,28 +15,44 @@ V = np.concatenate((V_slack, V_pq, V_pv)) #Programa válido para redes con un so
 S_pq=np.array([-4-2.5j])
 P_pv=np.array([2])
 
+for n in range(0, 3):
+    c=0
+    sumQ=0
+    while c<Y.shape[0]:
+        sumQ = sumQ + Y[Y.shape[0]-1][c]*V[c]
+        c+=1
+    S_pv = np.array([P_pv[0]-np.imag(sumQ*np.conjugate(V[Y.shape[0]-1]))*1j])
+    S_i = np.concatenate((S_pq, S_pv))
+    for i in range(2, Y.shape[0]+1):
+        sum1, sum2 =0,0
+        for j in range(1, i):
+            sum1=sum1+Y[i-1][j-1]*V[j-1]
+
+        for j in range(i+1, Y.shape[0]+1):
+            sum2=sum2+Y[i-1][j-1]*V[j-1]
+
+        V_i=(np.conjugate(S_i[i-2])/np.conjugate(V[i-1])-sum1-sum2)/Y[i-1][i-1]
+        V[i-1]=V_i
+    
+    I_slack = 0 + 0j
+    for a in range(Y.shape[0]):
+        I_slack += Y[0, a] * V[a]
+    S_slack = V_slack * np.conjugate(I_slack)
+    
+    print(f"Iteración {n+1}:")
+    for t in range(1, Y.shape[0]+1):
+        if t==1:
+            print(f"Nodo slack: V_{t}={V[t-1]:.4f}", "------------------ ",f"S_1={S_slack[0]:.4f}")
+        elif 1<t<Y.shape[0]:
+            print(f"Nodo PV: V_{t}={V[t-1]:.4f}", "---------------------- ",f"S_2={S_i[t-2]:.4f}")
+        elif t==Y.shape[0]:
+            print(f"Nodo PQ: V_{t}={V[t-1]:.4f}", "----------------------",f"S_3={S_i[t-4]:.4f}")
+        
+    V[Y.shape[0]-1]=V_pv[0]*(V[Y.shape[0]-1]/np.abs(V[Y.shape[0]-1]))
+    print(f"Nodo PQ: V_{Y.shape[0]} correcto= {V[Y.shape[0]-1]:.4f}")
+    print("")
+    V = np.round(V, 4)
 
 
-c=0
-sumQ=0
-while c<Y.shape[0]:
-    sumQ = sumQ + Y[Y.shape[0]-1][c]*V[c] 
-    c+=1
-S_pv = np.array([P_pv[0]-np.imag(sumQ*np.conjugate(V_pv[0]))*1j])
 
-S_i = np.concatenate((S_pq, S_pv))    
-
-for i in range(2, Y.shape[0]+1):
-    sum1, sum2 =0,0
-    for j in range(1, i):
-        sum1=sum1+Y[i-1][j-1]*V[j-1]
-
-    for j in range(i+1, Y.shape[0]+1):
-        sum2=sum2+Y[i-1][j-1]*V[j-1]
-
-    V_i=(np.conjugate(S_i[i-2])/V[i-1]-sum1-sum2)/Y[i-1][i-1]
-
-    V[i-1]=V_i
-
-print(np.round(V, 4))
 
