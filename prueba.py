ccbb090a3547d0fa04b5988d3ae49d998b375172
EduVillaa <eduardovllvrd@gmail.com>
2025-11-20 17:20:59 +0100

    Update PrácticaSEP.py

--- prueba.py
+++ prueba.py
@@ -20,26 +20,25 @@ P_pv=np.array([2])
 for n in range(0, numero_iteraciones+1):
     c=0
     sumQ=0
+
     while c<Y.shape[0]:
         sumQ = sumQ + Y[Y.shape[0]-1][c]*V[c]
         c+=1
     S_pv = np.array([P_pv[0]-np.imag(sumQ*np.conjugate(V[Y.shape[0]-1]))*1j])
     S_i = np.concatenate((S_pq, S_pv))
- 
     I_slack = 0 + 0j
     for a in range(Y.shape[0]):
         I_slack += Y[0, a] * V[a]
     S_slack = V_slack * np.conjugate(I_slack)
-    
     print(f"Iteración {n}:")
     for t in range(1, Y.shape[0]+1):
          if t==1:
             print(f"Nodo slack: V_{t} = {V[t-1]:.4f}", "------------------ ",f"S_{t}={S_slack[0]:.4f}")
          elif 1<t<Y.shape[0]:
-            print(f"Nodo PV: V_{t} = {V[t-1]:.4f}", "---------------------- ",f"S_{t}={S_i[t-2]:.4f}")
+            print(f"Nodo PQ: V_{t} = {V[t-1]:.4f}", "---------------------- ",f"S_{t}={S_i[t-2]:.4f}")
          elif t==Y.shape[0]:
-            print(f"Nodo PQ: V_{t} correcto = {V[t-1]:.4f}", "-----------",f"S_{t}={S_i[t-4]:.4f}")
-            
+            print(f"Nodo PV: V_{t} correcto = {V[t-1]:.4f}", "-----------",f"S_{t}={S_i[t-4]:.4f}")
+        
     if n>0:
         sum3=0    
         for z in range(0, Y.shape[0]):
@@ -60,13 +59,9 @@ for n in range(0, numero_iteraciones+1):
     
     V_PU_sin_corregir.append(V[Y.shape[0]-1])    
     if n>0:
-        print(f"Nodo PQ: V_{Y.shape[0]} sin corregir = {V_PU_sin_corregir[n-1]:.4f}")
+        print(f"Nodo PV: V_{Y.shape[0]} sin corregir = {V_PU_sin_corregir[n-1]:.4f}")
 
     V[Y.shape[0]-1]=V_pv[0]*(V[Y.shape[0]-1]/np.abs(V[Y.shape[0]-1]))
     print("")
-    #V = np.round(V, 4)
+    V = np.round(V, 4)
     
-
-
-
-
